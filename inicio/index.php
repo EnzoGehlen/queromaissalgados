c0e6bed9c48fa2d3eb13f81a3f08a867d64fdf74
EnzoGehlen <desenvolvimento01@seifai.edu.br>
2020-02-14 22:00:32 -0300

    first an unique

---
+++ inicio/index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>QUERO MAIS Salgados</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Vidaloka&display=swap" rel="stylesheet">
+    <link href="../css/uikit.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/font-awesome.css">
+
+</head>
+
+<body>
+    <div class="uk-section uk-padding-remove-bottom uk-padding-remove-top uk-section-default">
+        <!-- NAV -->
+        <div style='background-color: #FFF; z-index: 999999999' class="nav menu_topo" uk-sticky="bottom: #offset">
+            <div class="uk-container">
+                <nav class="uk-navbar uk-navbar-container uk-navbar-transparent" data-uk-navbar>
+                    <div class="uk-navbar-left">
+                        <div class="uk-navbar-item uk-padding-remove-horizontal">
+                            <a class="uk-logo uk-padding-small" title="QUERO MAIS Salgados" href=""><img width='20%'
+                                    height='20%' src="../img/logo.png" alt="QUERO MAIS Salgados"></a>
+                        </div>
+                    </div>
+                    <div class="uk-navbar-center">
+                        <ul class="uk-navbar-nav uk-visible@s">
+
+                            <li><a class="uk-button uk-button-text" href="">Onde será?</a></li>
+
+                            <li><a class="uk-button uk-button-text" href="">Quem somos?</a></li>
+                            <li><a class="uk-button uk-button-text" href="">O que faremos?</a></li>
+                            <li><a class="uk-button uk-button-text" href="">Contato</a></li>
+                            <li class='uk-margin-large-left'><a class=' uk-left uk-button-default uk-button-salmao'
+                                    href="">Faça aqui sua inscrição</a></li>
+                        </ul>
+                        <a class="uk-navbar-toggle uk-navbar-item uk-hidden@s" data-uk-toggle data-uk-navbar-toggle-icon
+                            href="#offcanvas-nav"></a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="uk-position-relative  uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: push">
+
+            <ul class="uk-slideshow-items">
+                <li>
+                    <div class="uk-position-cover uk-animation-reverse uk-transform-origin-center-left">
+                        <img src="http://webdesign-finder.com/kruton/wp-content/uploads/2018/12/slide_01.jpg" alt=""
+                            uk-cover>
+                        <div
+                            class="uk-visible@s uk-position-center caixaBanner uk-position-small uk-text-center uk-light">
+                            <div class='uk-position-top-center'>
+                                <p class="chamada-banner uk-padding-bottom">Cozinhar bem não precisa ser um bicho de
+                                    sete
+                                    cabeças, nós
+                                    vamos provar isso!</p>
+                                <h1 class="uk-text-center turmaTexto uk-margin-bottom">Próxima turma começa em:</h1>
+
+
+                            </div>
+                            <div class='uk-position-bottom-center'>
+                                <div class="uk-grid-small uk-text-center uk-child-width-auto" uk-grid
+                                    uk-countdown="date: 2020-03-20T07:00:00+00:00">
+                                    <div>
+                                        <div class="uk-countdown-number uk-countdown-days"></div>
+                                        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">Dias
+                                        </div>
+                                    </div>
+                                    <div class="uk-countdown-separator">:</div>
+                                    <div>
+                                        <div class="uk-countdown-number uk-countdown-hours"></div>
+                                        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">
+                                            Horas
+                                        </div>
+                                    </div>
+                                    <div class="uk-countdown-separator">:</div>
+                                    <div>
+                                        <div class="uk-countdown-number uk-countdown-minutes"></div>
+                                        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">
+                                            Minutos
+                                        </div>
+                                    </div>
+                                    <div class="uk-countdown-separator">:</div>
+                                    <div>
+                                        <div class="uk-countdown-number uk-countdown-seconds"></div>
+                                        <div class="uk-countdown-label uk-margin-small uk-text-center uk-visible@s">
+                                            Segundos
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="uk-position-cover  uk-animation-reverse uk-transform-origin-top-right">
+                        <img src="http://webdesign-finder.com/kruton/wp-content/uploads/2018/12/slide_02.jpg" alt=""
+                            uk-cover>
+                    </div>
+                </li>
+                <li>
+                    <div class="uk-position-cover  uk-animation-reverse uk-transform-origin-bottom-left">
+                        <img src="http://webdesign-finder.com/kruton/wp-content/uploads/2018/12/slide_03.jpg" alt=""
+                            uk-cover>
+                    </div>
+                </li>
+            </ul>
+            <div style='bottom:-30px; z-index: 99999;'
+                class="uk-position-bottom-center uk-padding-top uk-position-medium uk-position-z-index uk-text-center">
+                <a href="#ancora" style="color:gray;" data-uk-scroll="duration: 500"
+                    data-uk-icon="icon: arrow-down; ratio: 2" class="uk-icon"><svg width="40" height="40"
+                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="arrow-down">
+                        <polygon points="10.5,16.08 5.63,10.66 6.37,10 10.5,14.58 14.63,10 15.37,10.66"></polygon>
+                        <line fill="none" stroke="#000" x1="10.5" y1="4" x2="10.5" y2="15"></line>
+                    </svg></a>
+            </div>
+            <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous
+                uk-slideshow-item="previous"></a>
+            <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next
+                uk-slideshow-item="next"></a>
+
+
+            <div class="half-circle-line bottom-line">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="308px" height="41px"
+                    viewBox="0 0 308.000000 41.000000" preserveAspectRatio="xMidYMid meet">
+                    <g transform="translate(0.000000,41.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+                        <path
+                            d="M1280 395 c-174 -30 -287 -70 -558 -199 -271 -129 -410 -171 -617 -185 -61 -5 585 -8 1435 -8 850 0 1498 3 1440 7 -212 15 -344 54 -625 187 -285 135 -382 169 -560 198 -111 18 -409 18 -515 0z">
+                        </path>
+                    </g>
+
+                </svg>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+    <div class="uk-section uk-padding-remove-top"
+        style="background-image: url(../img/6194.png); background-repeat: repeat;">
+
+
+        <div class="uk-container">
+
+            <div class="uk-section  uk-section-default" style="background-color: rgba(0,0,0,0.0)">
+
+                <h1 id='ancora' class="uk-text-center uk-padding-bottom uk-margin-bottom">Conheça os lugares e pessoas
+                    que
+                    são parte deste
+                    projeto</h1>
+                <div class="uk-text-center uk-padding uk-child-width-1-3@m">
+                    <span class='uk-text-emphasis' uk-icon="icon: location; ratio: 2"></span>
+
+                </div>
+
+
+                <div class="uk-child-width-1-2@m uk-padding-top" uk-grid>
+                    <div>
+                        <div class="uk-card uk-card-default">
+                            <div class="uk-card-media-top">
+                                <img width="100%" height="100%" src="../img/tp.jpg" alt="">
+                            </div>
+                            <div class="uk-card-body">
+
+                                <h3 class="uk-card-title">Três Passos - RS</h3>
+                                <p>Alguma breve descrição com local e/ou data
+                                </p>
+                                <div class="uk-card-footer">
+                                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <img class="uk-border-circle" width="50" height="50" src="../img/carol.png">
+                                        </div>
+                                        <div class="uk-width-expand uk-padding-remove-bottom">
+                                            <h4 class=" uk-margin-remove-bottom">Caroline Gehlen</h4>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div class="uk-card uk-card-default">
+
+                            <div class="uk-card-media-top">
+                                <img width="100%" height="100%" src="../img/rondon.jpg" alt="">
+                            </div>
+                            <div class="uk-card-body">
+
+                                <h3 class="">Mal. Cândido Rondon - PR</h3>
+                                <p>Alguma breve descrição com local e/ou data
+                                </p>
+                                <div class="uk-card-footer">
+                                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                                        <div class="uk-width-auto">
+                                            <img class="uk-border-circle" width="50" height="50"
+                                                src="../img/adelaide.png">
+                                        </div>
+                                        <div class="uk-width-expand uk-padding-remove-bottom">
+                                            <h4 class=" uk-margin-remove-bottom">Adelaide Lüetkmeyer</h4>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="uk-section uk-margin-remove-bottom uk-padding-remove-top uk-padding-remove-bottom">
+
+        <div class=" uk-card-salmao uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin-remove-bottom uk-margin"
+            uk-grid>
+            <div class="uk-card-media-left uk-cover-container">
+                <img src="../img/video-image.jpg" alt="" uk-cover>
+                <canvas width="600" height="600"></canvas>
+            </div>
+            <div>
+                <div class="uk-card-body  uk-padding-large">
+                    <h1 class="uk-padding uk-heading-large culinaria ">A culinária de uma forma que você nunca viu!</h1>
+                    <div style='background-color: rgba(255,128,128, 0.2)' class="uk-padding">
+                        <p class="items"><i style='color: coral;'
+                                class="fa fa-calendar fa-2x uk-padding-small"></i></i>7 DIAS DE CURSO INTENSIVO;
+                            <p class="items"><i style='color: coral;'
+                                    class="fa fa-certificate fa-2x uk-padding-small"></i></i>CERTIFICADO E APOSTILA COM
+                                TODAS AS RECEITAS ENTREGUE NO FINAL DO CURSO;
+                                <p class="items"><i style='color: coral;'
+                                        class="fa fa-cutlery fa-2x uk-padding-small"></i></i>AULAS PRÁTICAS COM RECEITAS
+                                    REAIS
+
+                    </div>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="uk-margin-remove-top uk-card-salmao uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin"
+            uk-grid>
+
+            <div>
+                <div class="uk-card-body  uk-padding-large">
+                    <p class='uk-margin-remove-bottom depoimento'>Depoimento da autora</p>
+                    <h1 class="uk-margin-remove-top uk-heading-large depoimento">Uma oportunidade para você</h1>
+                    <p class="uk-padding uk-text-lead">"Acredito que a culinária seja uma forma de arte que utilizamos
+                        todos os 5
+                        sentidos para apreciar..."</p>
+                    <div style='background-color: rgba(255,128,128, 0.2)' class="uk-padding">
+                        <p class="escrita">
+                            O meu objetivo é ensinar técnicas e dicas para que cada pessoa ao fazer este curso
+                            possa inovar e ser capaz de criar suas próprias receitas, tanto para usufruir como um hobby
+                            quanto profissionalmente. Podendo assim
+                            ter uma renda extra, aumentando sua renda familiar.
+
+                            <img class='uk-float-right' width='30%' src="../img/assinatura.png" alt="">
+                        </p>
+
+
+
+                    </div>
+
+                    <hr class="uk-divider-icon">
+
+
+                    <h2 class="uk-margin-remove-bottom uk-heading-large depoimento">Teresinha Maria Lüetkmeyer</h2>
+                    <p class='uk-margin-remove-top depoimento'>Autora do curso</p>
+
+
+
+
+                </div>
+            </div>
+            <div class="uk-card-media-right uk-cover-container">
+                <img src="../img/video-image.jpg" alt="" uk-cover>
+                <canvas width="600" height="600"></canvas>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="uk-section uk-margin-remove-top"
+        style="background-image: url(../img/6194.png); background-repeat: repeat;">
+
+
+        <div class="uk-container">
+
+            <div class="uk-section  uk-section-default" style="background-color: rgba(0,0,0,0.0)">
+                <p class='uk-margin-remove-bottom uk-text-center depoimento'>Agenda atualizada</p>
+                <h1 class="uk-margin-remove-top uk-margin-remove-bottom uk-text-center uk-heading-large depoimento">Veja uma prévia de cada dia
+                </h1>
+                <div class="uk-text-center uk-margin-remove-top uk-padding uk-child-width-1-3@m">
+                    <span  class="uk-icon  uk-height-small uk-text-center uk-icon-image"
+                        style="background-image: url(../img/icon-main2.png); background-size: 35px 35px;"></span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="../js/uikit.js"></script>
+    <script src="../js/uikit-icons.js"></script>
+</body>
+
+</html>
